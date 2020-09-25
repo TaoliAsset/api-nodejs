26f6e757724aa50f9509c19188512101537ee66f
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-09-25 11:21:37 +0800

    DT.ts

--- src/DT.ts
+++ src/DT.ts
@@ -28,8 +28,6 @@ import BasicNull from "./BasicNull";
 import BasicSymbol from "./BasicSymbol";
 import BasicChar from "./BasicChar";
 
-let DtCt = new Object();
-
 function arrayLize(value, ftypect) {
   if (value instanceof BasicScalar) return value;
   if (!(value instanceof Array)) return ftypect(value);
@@ -40,171 +38,172 @@ function arrayLize(value, ftypect) {
   return v;
 }
 
-DtCt.Bool = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicBool(v);
-  });
-};
+namespace DtCt {
+  function Bool(value) {
+    return arrayLize(value, function (v) {
+      return new BasicBool(v);
+    });
+  }
 
-DtCt.Char = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicChar(v);
-  });
-};
+  function Char(value) {
+    return arrayLize(value, function (v) {
+      return new BasicChar(v);
+    });
+  }
 
-DtCt.Byte = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicByte(v);
-  });
-};
+  function Byte(value) {
+    return arrayLize(value, function (v) {
+      return new BasicByte(v);
+    });
+  }
 
-DtCt.Short = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicShort(v);
-  });
-};
+  function Short(value) {
+    return arrayLize(value, function (v) {
+      return new BasicShort(v);
+    });
+  }
 
-DtCt.Int = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicInt(v);
-  });
-};
+  function Int(value) {
+    return arrayLize(value, function (v) {
+      return new BasicInt(v);
+    });
+  }
 
-DtCt.Long = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicLong(v);
-  });
-};
+  function Long(value) {
+    return arrayLize(value, function (v) {
+      return new BasicLong(v);
+    });
+  }
 
-DtCt.Date = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicDate(v);
-  });
-};
+  function Date(value) {
+    return arrayLize(value, function (v) {
+      return new BasicDate(v);
+    });
+  }
 
-DtCt.Month = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicMonth(v);
-  });
-};
+  function Month(value) {
+    return arrayLize(value, function (v) {
+      return new BasicMonth(v);
+    });
+  }
 
-DtCt.Time = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicTime(v);
-  });
-};
+  function Time(value) {
+    return arrayLize(value, function (v) {
+      return new BasicTime(v);
+    });
+  }
 
-DtCt.Minute = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicMinute(v);
-  });
-};
+  function Minute(value) {
+    return arrayLize(value, function (v) {
+      return new BasicMinute(v);
+    });
+  }
 
-DtCt.Second = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicSecond(v);
-  });
-};
+  function Second(value) {
+    return arrayLize(value, function (v) {
+      return new BasicSecond(v);
+    });
+  }
 
-DtCt.DateTime = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicDateTime(v);
-  });
-};
+  function DateTime(value) {
+    return arrayLize(value, function (v) {
+      return new BasicDateTime(v);
+    });
+  }
 
-DtCt.TimeStamp = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicTimeStamp(v);
-  });
-};
+  function TimeStamp(value) {
+    return arrayLize(value, function (v) {
+      return new BasicTimeStamp(v);
+    });
+  }
 
-DtCt.NanoTime = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicNanoTime(v);
-  });
-};
+  function NanoTime(value) {
+    return arrayLize(value, function (v) {
+      return new BasicNanoTime(v);
+    });
+  }
 
-DtCt.NanoTimeStamp = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicNanoTimeStamp(v);
-  });
-};
+  function NanoTimeStamp(value) {
+    return arrayLize(value, function (v) {
+      return new BasicNanoTimeStamp(v);
+    });
+  }
 
-DtCt.Float = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicFloat(v);
-  });
-};
+  function Float(value) {
+    return arrayLize(value, function (v) {
+      return new BasicFloat(v);
+    });
+  }
 
-DtCt.Double = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicDouble(v);
-  });
-};
+  function Double(value) {
+    return arrayLize(value, function (v) {
+      return new BasicDouble(v);
+    });
+  }
 
-DtCt.Vector = function (value) {
-  return new BasicVector(value);
-};
+  function Vector(value) {
+    return new BasicVector(value);
+  }
 
-DtCt.Pair = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicPair(v);
-  });
-};
+  function Pair(value) {
+    return arrayLize(value, function (v) {
+      return new BasicPair(v);
+    });
+  }
 
-DtCt.Matrix = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicMat(v);
-  });
-};
+  function Matrix(value) {
+    return arrayLize(value, function (v) {
+      return new BasicMat(v);
+    });
+  }
 
-DtCt.Set = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicSet(v);
-  });
-};
+  function Set(value) {
+    return arrayLize(value, function (v) {
+      return new BasicSet(v);
+    });
+  }
 
-DtCt.Dict = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicDict(v);
-  });
-};
+  function Dict(value) {
+    return arrayLize(value, function (v) {
+      return new BasicDict(v);
+    });
+  }
 
-DtCt.Table = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicTable(v);
-  });
-};
+  function Table(value) {
+    return arrayLize(value, function (v) {
+      return new BasicTable(v);
+    });
+  }
 
-DtCt.UUID = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicUuid(v);
-  });
-};
+  function UUID(value) {
+    return arrayLize(value, function (v) {
+      return new BasicUuid(v);
+    });
+  }
 
-DtCt.IpAddr = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicIpAddr(v);
-  });
-};
+  function IpAddr(value) {
+    return arrayLize(value, function (v) {
+      return new BasicIpAddr(v);
+    });
+  }
 
-DtCt.Int128 = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicInt128(v);
-  });
-};
+  function Int128(value) {
+    return arrayLize(value, function (v) {
+      return new BasicInt128(v);
+    });
+  }
 
-DtCt.Null = function (value) {
-  // value is data type
-  return arrayLize(value, function (v) {
-    return new BasicNull(v);
-  });
-};
+  function Null(value) {
+    // value is data type
+    return arrayLize(value, function (v) {
+      return new BasicNull(v);
+    });
+  }
 
-DtCt.Symbol = function (value) {
-  return arrayLize(value, function (v) {
-    return new BasicSymbol(v);
-  });
-};
-
+  function Symbol(value) {
+    return arrayLize(value, function (v) {
+      return new BasicSymbol(v);
+    });
+  }
+}
 export default DtCt;
